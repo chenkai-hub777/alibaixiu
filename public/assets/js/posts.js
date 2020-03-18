e03d9f289a6ef0de2af452fd47ce5e43b5241ab2
chenkai <1615802318@qq.com>
2020-03-18 10:12:50 +0800

    完成了文章分页效果

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -7,8 +7,28 @@ $.ajax({
         page:1
     },
     success:function(res){
-        // console.log(res);
+        console.log(res);
         let html = template('pTpl',{data:res.records});
-        $('tbody').html(html)
+        $('tbody').html(html);
+        let pageHtml = template('pageTpl',res);
+        $('.pagination').html(pageHtml);
     }
-})
+});
+
+// 定义一个函数，点击分页
+function changePage(index){
+    $.ajax({
+        type:'get',
+        url:'/posts',
+        data:{
+            page:index
+        },
+        success:function(res){
+            console.log(res);
+            let html = template('pTpl',{data:res.records});
+            $('tbody').html(html);
+            let pageHtml = template('pageTpl',res);
+            $('.pagination').html(pageHtml);
+        }
+    });
+}
